cb3cc20b06b0f0744074d1018e22f42bc85ce096
Alan Rogers <rogers@ouzel>
2014-05-17 15:53:20 -0600

    Make sasimplex use conventional parameters when state space has a single dimension. sald still doesn't work with model of constant population size.

--- src/sasimplex.c
+++ src/sasimplex.c
@@ -20,7 +20,7 @@
  * 02110-1301, USA.
  */
 
-/**
+/*
  * 2014-04-07: sasimplex.c and sasimplex.h implement Simplex Simulated
  *             Annealing, as described in Numerical Recipes, by Press
  *             et al. This implementation does not use code from
@@ -942,10 +942,17 @@ sasimplex_onestep(void *vstate, gsl_multimin_function * func,
      * Han (Computational Optimization and Applications
      * 51(1):259-277, 2012).
      */
-    const double alpha = 1.0;
-    const double beta = 1.0 + 2.0 / n;
-    const double gmma = 0.75 - 1.0 / (2.0 * n);
-    const double delta = 1.0 - 1.0 / n;
+    double alpha = 1.0;
+    double beta = 2.0;
+	double gmma = 0.5;
+	double delta = 0.5;
+
+	if(n > 2) {
+		alpha = 1.0;
+		beta = 1.0 + 2.0 / n;
+		gmma = 0.75 - 1.0 / (2.0 * n);
+		delta = 1.0 - 1.0 / n;
+	}
 
     /*
      * Find highest, second highest and lowest point. We need the
