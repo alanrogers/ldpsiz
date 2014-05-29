e2213606f1c5e2c836cc0ca9e26fe47e1c04365a
Alan R. Rogers <rogers@anthro.utah.edu>
2014-05-29 12:00:57 -0600

    Still working on doxygen references.

--- src/ms2gtp.c
+++ src/ms2gtp.c
@@ -1,6 +1,6 @@
 /**
  * @file ms2gtp.c
- * @page ms2gtp
+ * @anchor ms2gtp
  * @brief This file contains code related ms2gtp, a program that
  * translates output from Dick Hudson's `ms` program into .gtp 
  * format.    
